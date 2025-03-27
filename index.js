957390640c466113c19f8e4dc30ce9ffddfea757
Elvis <elvis.wachira@student.moringaschool.com>
2025-03-27 20:45:31 +0300

    new changes

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+//.Write your code here
+const main = document.querySelector('main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Your-Name is the champion!';
+
+document.body.appendChild(newHeader);
